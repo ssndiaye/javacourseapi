c32f5ef204e59a7c997b9e8d9e04ddf9373a7fb9
saliou <saliou.ndiaye2007@gmail.com>
2020-09-02 22:03:33 -0400

    1st commit

---
+++ main/java/io/springboot/hello/HelloController.java
@@ -0,0 +1,22 @@
+package io.springboot.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+
+
+@RestController
+public class HelloController {
+	
+	
+	
+	@RequestMapping("/hello")
+	public String sayHi() {
+
+		return "Hi" ;
+	}
+	
+
+}
